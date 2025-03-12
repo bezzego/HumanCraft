736b7225022e55e192ac83b27640b8ae62c0d867
Egor Bezborodov <egor.bezborodoff@gmail.com>
2025-03-12 22:22:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+from faker import Faker
+import file_operations
+import random
+import os
+
+
+LETTERS_MAPPING = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠', 'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠', 'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒', 'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠', 'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋', 'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋', 'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е', 'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋', 'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠', 'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋', 'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋', 'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+SKILLS = [
+    "Стремительный прыжок", "Электрический выстрел", "Ледяной удар",
+    "Стремительный удар", "Кислотный взгляд", "Тайный побег",
+    "Ледяной выстрел", "Огненный заряд"
+]
+
+
+def generate_runic_skills(skills):
+    return ["".join(LETTERS_MAPPING.get(char, char) for char in skill) for skill in skills]
+
+
+def generate_character(fake, runic_skills):
+    return {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": random.choice(runic_skills),
+        "skill_2": random.choice(runic_skills),
+        "skill_3": random.choice(runic_skills)
+    }
+
+
+def main():
+    fake = Faker("ru_RU")
+    runic_skills = generate_runic_skills(SKILLS)
+    
+    results_dir = os.path.join("Results")
+    os.makedirs(results_dir, exist_ok=True)
+    
+    for i in range(10):
+        context = generate_character(fake, runic_skills)
+        filename = os.path.join(results_dir, f"result_{i+1}.svg")
+        file_operations.render_template("charsheet.svg", filename, context)
+
+
+if __name__ == '__main__':
+    main()
